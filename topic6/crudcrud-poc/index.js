12ec211cefdb6b80dfaa99bac9003008c1b492ba
Mark Szymanski <markjszy@gmail.com>
2021-02-26 10:59:37 -0500

    crudcrud demo

---
+++ topic6/crudcrud-poc/index.js
@@ -0,0 +1,66 @@
+let baseUrl = 'https://crudcrud.com/api/48997f60c66742d6a3361e06f7b173a5/houses'; // houses is our chosen resource
+
+$('#getResource').on('click', () => {
+    $.ajax({
+        url: baseUrl,
+        method: 'GET'
+    }).then(results => {
+        $('#getResults').html(`<code>${JSON.stringify(results)}</code>`);
+    })
+});
+
+$('#postResource').on('click', () => {
+    $.ajax({
+        url: baseUrl,
+        method: 'POST',
+        datatype: 'json',
+        data: JSON.stringify({'name': 'My house', 'created': `${Date.now()}`}),
+        contentType: 'application/json'
+    }).then(results => {
+        $('#postResults').html(`<code>${JSON.stringify(results)}</code>`);
+    })
+});
+
+// for the put request, we'll just grab
+// the id of the last resource and make a trivial change to it
+$('#putResource').on('click', () => {
+    $.ajax({
+        url: baseUrl,
+        method: 'GET'
+    }).then(results => {
+        if (results.length) {
+            let lastResult = results[results.length-1];
+            lastResult.name = 'Altered name!';
+            $.ajax({
+                url: `${baseUrl}/${lastResult._id}`,
+                method: 'PUT',
+                datatype: 'json',
+                data: JSON.stringify(lastResult),
+                contentType: 'application/json'
+            }).then(putResult => {
+                $('#putResults').html(`<code>${JSON.stringify(putResult)}</code>`);
+            })
+        }
+    });
+});
+
+// for the delete request, we'll delete the last resource
+$('#deleteResource').on('click', () => {
+    $.ajax({
+        url: baseUrl,
+        method: 'GET'
+    }).then(results => {
+        if (results.length) {
+            let lastResult = results[results.length-1];
+            lastResult.name = 'Altered name!';
+            $.ajax({
+                url: `${baseUrl}/${lastResult._id}`,
+                method: 'DELETE',
+                datatype: 'json',
+                contentType: 'application/json'
+            }).then(deleteResult => {
+                $('#deleteResults').html(`<code>${JSON.stringify(deleteResult)}</code>`);
+            })
+        }
+    });
+});
